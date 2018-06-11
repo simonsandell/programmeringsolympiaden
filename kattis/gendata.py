98a1e9f8e8d1e8f041701040c2ccd97a641652a5
simonsandell <simonsandell@live.se>
2018-06-11 09:10:37 +0200

    asdf

--- kattis/gendata.py
+++ kattis/gendata.py
@@ -1,17 +1,18 @@
 import random
-random.seed();
+random.seed()
 
-of = open("./input.in","w");
-N = random.randint(3,pow(10,3));
-H = random.randint(1,pow(10,3));
-x_old = 0;
-of.write(str(N)+" "+str(H)+"\n");
-of.write("0 0 " +str(random.randint(0,1))+"\n");
+of = open("./input.in","w")
+N = random.randint(3,pow(10,5))
+H = random.randint(1,pow(10,6))
+x_old = 0
+of.write(str(N)+" "+str(H)+"\n")
+of.write("0 0 " +str(random.randint(0,1))+"\n")
+
 for i in range(N-2):
-    x = random.randint(0,10);
-    y = random.randint(0,H);
-    z = random.randint(0,1);
-    of.write(str(x+x_old)+" "+str(y)+" "+str(z)+"\n");
-    x_old += x;
-xmax = x_old + random.randint(0,10);
-of.write(str(xmax)+" 0 "+str(random.randint(0,1))+"\n");
+    x = random.randint(0,10)
+    y = random.randint(0,H)
+    z = random.randint(0,1)
+    of.write(str(x+x_old)+" "+str(y)+" "+str(z)+"\n")
+    x_old += x
+xmax = x_old + random.randint(0,10)
+of.write(str(xmax)+" 0 "+str(random.randint(0,1))+"\n")
--- kattis/kattis.py
+++ kattis/kattis.py
@@ -1,34 +1,14 @@
-import matplotlib.pyplot as plt
 import time
 import itertools
-
-def prepare_trials(num_to_cov, num_guards, big_dict,length_dict):
-    for x in itertools.combinations(length_dict.keys(),num_guards):
-        s = 0;
-        for key in x:
-            s+= length_dict[key]
-        if s >= num_to_cov:
-            act_cov = set()
-            for key in x:
-                act_cov = act_cov.union(big_dict[key])
-            if len(act_cov) == num_to_cov:
-                return num_guards
-    return prepare_trials(num_to_cov, num_guards + 1, big_dict,length_dict)
-
-
-
-
-
-
-
+import matplotlib.pyplot as plt
 
 # find the interval of x which can be seen from a certain point
 
-def getSeeableInterval(n,H,X,Y,x,y):
+def getSeeableInterval(n, H, X, Y, x, y):
     right_lim = X[-1]
     hy = H-y
 
-    for i in range(n+1,len(X)):
+    for i in range(n+1, len(X)):
         if X[i] > right_lim:
             break
         dy = Y[i] -y
@@ -40,7 +20,7 @@ def getSeeableInterval(n,H,X,Y,x,y):
                 right_lim = chi
     left_lim = 0
 
-    for i in reversed(range(0,n)):
+    for i in reversed(range(0, n)):
         if X[i] < left_lim:
             break
         dy = Y[i] -y
@@ -52,6 +32,7 @@ def getSeeableInterval(n,H,X,Y,x,y):
                 left_lim = chi
 
     return (left_lim,right_lim)
+    #return (int(0.5 + left_lim), int(-0.5 + right_lim))
 #
 # N number of wall points
 # H y-distance of the wall
@@ -60,81 +41,91 @@ def getSeeableInterval(n,H,X,Y,x,y):
 # Z 1 or 0 if point must be covered
 #
 def kattis(N, H, X, Y, Z):
-    points_to_cover = set()
+    print("function called")
+    points_to_cover = []
     num_to_cover = 0
     cov_points = {}
     points_of_interest = set()
-    # for each point we want to cover, get the x intervals (x_start,x_end)
+    # for each point we want to cover, get the x intervals (x_start, x_end)
     # on the wall from which it can be seen,
     # make set of points where we might want to place guards
-    plt.plot(X,Y)
-    plt.plot([X[0],X[-1]],[H,H])
+    plt.plot(X, Y)
+    plt.plot([X[0], X[-1]], [H, H])
 
 
-    for i in range(len(X)):
+    for i,x in enumerate(X):
         if Z[i] == 1:
-            points_to_cover.add(X[i])
-            cov_points[X[i]] = getSeeableInterval(i,H,X,Y,X[i],Y[i])
+            points_to_cover.append(X[i])
+            cov_points[x] = getSeeableInterval(i, H, X, Y, x, Y[i])
             points_of_interest.add(cov_points[X[i]][0])
             points_of_interest.add(cov_points[X[i]][1])
     num_to_cover = len(points_to_cover)
-    print("generated seeableINtervals")
     pts_seen_from_poi = {}
 
     dict_of_setlengths = {}
+    pois_seeing_cov_pt = {}
+    print("seeable intervals generated")
+
+    for cp in cov_points:
+        pois_seeing_cov_pt[cp] = set()
+
+
     for poi in points_of_interest:
         seen_from_this = set()
-        doadd = True;
+        doadd = True
         # determine which points can be seen from this poi
-        for cov_pt in cov_points:
+
+        for cov_pt in points_to_cover:
             if ((poi <= cov_points[cov_pt][1]) and (poi >= cov_points[cov_pt][0])):
                 seen_from_this.add(cov_pt)
         # check if this set is subset of any other, if so, dont add
+
         for key in pts_seen_from_poi:
             if seen_from_this.issubset(pts_seen_from_poi[key]):
                 doadd = False
+
         if doadd:
-            pts_seen_from_poi[poi] = seen_from_this;
+            pts_seen_from_poi[poi] = seen_from_this
             dict_of_setlengths[poi] = len(seen_from_this)
 
-    list_of_setlengths = sorted(list(dict_of_setlengths.items()))
-    max_cov = 0;
-    min_num_guards = 0;
+            for cov_pt_seen in seen_from_this:
+                pois_seeing_cov_pt[cov_pt_seen].add(poi)
+    print("pois pruned")
+
+    list_of_setlengths = sorted(list(dict_of_setlengths.values()))
+    max_cov = 0
+    min_num_guards = 0
+
     while max_cov < num_to_cover:
         max_cov += list_of_setlengths[-1]
         del list_of_setlengths[-1]
         min_num_guards += 1
 
-    print(min_num_guards)
 
+    n_guards = 0
+    already_seen = set()
 
-    # now, do bredden forst sokning av tradet
-    # trials = [ [set(),n_guards],...]
-    
-    trials = prepare_trials(num_to_cover, min_num_guards, pts_seen_from_poi,dict_of_setlengths)
-    return trials
-"""
-    for poi in pts_seen_from_poi:
-        trials.append([poi])
+    while len(pois_seeing_cov_pt) > 0:
+        print(len(pois_seeing_cov_pt))
+        # get a point to be covered and the set of pois seeing that point
+        cov_pt,poi_seeing_cov_pt = pois_seeing_cov_pt.popitem()
+        already_seen.add(cov_pt)
+        max_cov = 0;
+        unseen_pts = set()
+        # check which of those pois sees the most cov_pts
 
-    while True:
-        # pick out first element and remove from list
-        try_guards = trials[0]
-        del trials[0]
-        pts_covered = set()
-        for g in try_guards:
-            pts_covered = pts_covered.union(pts_seen_from_poi[g])
+        for poi in poi_seeing_cov_pt:
+            poi_new_unseen = pts_seen_from_poi[poi].difference(already_seen)
+            n_poi_seen = len(poi_new_unseen)
 
-        print(try_guards)
-        if pts_covered == points_to_cover:
-            for g in try_guards:
-                plt.plot(g,H,'bo')
-            plt.show()
-            return len(try_guards)
-        else:
-            for poi in pts_seen_from_poi:
-                if not poi in try_guards:
-                    new_guards = try_guards.copy()
-                    new_guards.append(poi)
-                    trials.append(new_guards)
-"""
+            if n_poi_seen > max_cov:
+                max_cov = n_poi_seen
+                unseen_pts = poi_new_unseen
+        already_seen = already_seen.union(unseen_pts)
+        # remove cov_pts seen by try guard 
+
+        for seen_pt in unseen_pts:
+            del pois_seeing_cov_pt[seen_pt]
+        n_guards += 1;
+
+    return n_guards
